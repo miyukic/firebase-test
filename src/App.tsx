1f97edf3297b8597dc7da55d74d0a85b04eda55a
miyukic <m.yukic10@gmail.com>
2022-01-08 23:51:03 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// https://qiita.com/takeshisakuma/items/823d035919b89020f4e1
+// Reactルーター参考ページ
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+// 各ページ
+import Page1 from './Page1';
+
+interface AppProps {
+    children?: string;
+}
+
+const App = (props: AppProps) => {
+    return (
+        <Router>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/Page1.tsx
@@ -0,0 +1,9 @@
+import react from 'react';
+
+const Page1 = () => {
+    return (
+        <div>Page1を開いていますです。</div>
+    )
+}
+
+export default Page1;
