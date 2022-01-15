9234a1e63aa96cd5ac4892eeab23202e651e8e1b
miyukic <m.yukic10@gmail.com>
2022-01-15 23:46:11 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,47 @@
 // Reactルーター参考ページ
 
 import './App.css';
-import { Link } from 'react-router-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-
-// 各ページ
-import Page1 from './Page1';
+import React, { useEffect, useState } from 'react';
+import { app } from './MyFirebase';
+import { addDoc, collection, DocumentData, Firestore, getDocs, getFirestore, QuerySnapshot } from 'firebase/firestore/lite';
 
 interface AppProps {
     children?: string;
 }
 
+const db: Firestore = getFirestore(app);
+
+//export async function getCities(db: Firestore) {
+//    const citiesCol = collection(db, 'cities');
+//    const p: Promise<QuerySnapshot<DocumentData>> = getDocs(citiesCol);
+//    const citySnapshot: QuerySnapshot<DocumentData> = await p;
+//    const cityList = citySnapshot.docs.map(doc => doc.data());
+//    return cityList;
+//}
+
+const clickButton = () => {
+    const id = '003'
+    const name = 'test'
+    const uid = ''
+
+    const db = getFirestore()
+    const docRef = addDoc(collection(db, 'tasks'), {
+      uid: uid,
+      id: id,
+      name: name
+    })
+    console.log('Document', docRef)
+}
+
 const App = (props: AppProps) => {
+    const [state, setState] = useState<string>();
+    useEffect(() => {
+    }, [])
     return (
-        <Router>
-        </Router>
+        <>
+        <p>{state}</p>
+        <button onClick={clickButton}>Firestore追加</button>
+        </>
     );
 }
 
