125e53b7022e970162b63614170b4c395c49ae42
miyukic <m.yukic10@gmail.com>
2022-01-18 04:08:02 +0900

    onChangeなどのイベントハンドラ向けの型定義の方法

--- src/App.tsx
+++ src/App.tsx
@@ -36,8 +36,22 @@ const App = (props: AppProps) => {
     const [state, setState] = useState<string>();
     const initText = "入力してください";
     const [text, setText] = useState<string>(initText);
+
     type rt = ReturnType<ChangeEventHandler<HTMLInputElement>>;
-    type para = Parameters<ChangeEventHandler<HTMLInputElement>>
+    //onChangeイベントハンドラの型から戻り値を取得できるはの最高！！
+    //だけど、onChangeの型を調べなければならないのが惜しい！
+
+    //type rt2 = ReturnType<typeof onChange>;
+    //イベントハンドラの名前から直接戻り値が取得できれば最高....！
+
+    type para = Parameters<ChangeEventHandler<HTMLInputElement>>;
+
+    /*
+    //onChangeの型
+    const ChangeEvent: ChangeEventHandler<HTMLInputEvent> = (e: ChangeEvent<HTMLInputElement>): void => {
+        // 処理
+    }
+    */
     const changeEvent: ChangeEventHandler<HTMLInputElement> = (e: para[0]): rt => {
         if (e == undefined) return;
         setText(() => e.target.value)
