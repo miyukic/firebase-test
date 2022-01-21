2d04202187c405a5e91987e5b5ef80dd59b31d45
miyukic <m.yukic10@gmail.com>
2022-01-21 23:48:07 +0900

    update

--- src/MyFirebase.ts
+++ src/MyFirebase.ts
@@ -3,13 +3,23 @@ import { getFirestore, collection, getDocs, Firestore,
     QuerySnapshot, CollectionReference, DocumentData } from 'firebase/firestore/lite';
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+    apiKey: "AIzaSyDfM9MLlNJFZi_EOVousSoThyqHkmjGS8E",
+    authDomain: "fir-nettest-4b20a.firebaseapp.com",
+    projectId: "fir-nettest-4b20a",
+    storageBucket: "fir-nettest-4b20a.appspot.com",
+    messagingSenderId: "899853734149",
+    appId: "1:899853734149:web:fd9b71de690344ebc4276c",
+    measurementId: "G-V1LXJLY124"
+
+    // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+    // appId: process.env.REACT_APP_FIREBASE_SENDER_ID,
 };
+
+console.log(firebaseConfig.measurementId);
 
 export const app = firebase.initializeApp(firebaseConfig);
 
